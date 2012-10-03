130d8e087c03a2ab8d7659ff29e1e4bb50f8078f
kjellan <kjell.anfinsen@gmail.com>
2012-10-03 10:10:07 +0200

    hei

--- testapp3/TestAppAppDelegate.m
+++ testapp3/TestAppAppDelegate.m
@@ -12,6 +12,8 @@
 
 @implementation TestAppAppDelegate
 
+// hallo
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
